6e725a78572762b245714719067a90d00e5ac908
Jacob Nash <jacob.matthew.nash@gmail.com>
2021-01-05 15:00:53 +0000

    added temperature calculation

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,28 @@
 int switchPin = 13;
 int motor = 3;
 int switchInput;
+int temperatureInputPin = A0;
+int LM35Input;
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(switchPin,INPUT);
   pinMode(motor, OUTPUT);
   digitalWrite(motor,LOW);
-  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  LM35Input= analogRead(temperatureInputPin);
+  float temp = calcTemp(LM35Input);
+  
+
   switchInput = digitalRead(switchPin);
   if(switchInput == 0){
     digitalWrite(motor, !digitalRead(motor));
   }
-  delay(100);
+  delay(1000);
+}
 
+float calcTemp(int input){
+  float res = (input/1024.0) * 500;
+  return res;
 }
